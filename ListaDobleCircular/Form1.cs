ebf099008c45d5fdae4a6bb04ba0ef95c0f2555e
hp <hp@DESKTOP-85MULR1>
2022-12-09 18:56:43 -0600

    Agregar archivos de proyecto.

---
+++ ListaDobleCircular/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaDobleCircular
+{
+    public partial class Form1 : Form
+    {
+        Lista miLista = new Lista();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Lista nueva= new Lista();
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            Nodo n= new Nodo();
+            n.Dato = int.Parse(txtdato.Text);
+            miLista.Agregar(n);
+            
+            //txtdato.Clear
+            miLista.ImprimirPU();
+            txtdato.Clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int dato = int.Parse(txtdato.Text);
+            miLista.Eliminar(dato);
+            miLista.ImprimirPU();
+            txtdato.Clear();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int dato = 0;
+            try
+            { dato = int.Parse(txtdato.Text); }
+            catch
+            { }
+            if (miLista.Buscar(dato))
+            {
+                MessageBox.Show("Encontrado");
+                return;
+            }
+            MessageBox.Show("No encontrado");
+            txtdato.Clear();
+        }
+        
+    }
+}
---
+++ ListaDobleCircular/Nodo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaDobleCircular
+{
+    internal class Nodo
+    {
+        private int dato;
+        private Nodo siguiente;
+        private Nodo atras;
+        public int Dato
+            { get { return dato; } set { dato = value; } }
+        public Nodo Siguiente 
+            { get { return siguiente; } set { siguiente = value;  } }
+        
+        public Nodo Atras 
+            { get { return atras; } set { atras = value;} }
+        public Nodo()
+        {
+            dato= 0;
+            siguiente = null;
+            atras = null;
+        }
+        public Nodo(int dato, Nodo siguiente, Nodo atras)
+        { 
+            this.dato = dato; 
+            this.siguiente = siguiente; 
+            this.atras = atras;
+        }
+        public override string ToString()
+        {
+            return dato + " - " ;
+        }
+    }
+}
